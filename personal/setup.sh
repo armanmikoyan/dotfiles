54d63ce399404018cff6913c85c1171f4bacb6aa
armanmikoyan <arman.mikoyan1@gmail.com>
2026-03-07 19:19:14 +0400

    feat: warn me when i missed .env to setup

--- personal/setup.sh
+++ personal/setup.sh
@@ -9,3 +9,9 @@ source "$DIR/config/nvm.sh"
 source "$DIR/config/aliases/shell.sh"
 source "$DIR/config/aliases/git.sh"
 source "$DIR/config/aliases/projects.sh"
+
+if [[ -f "$DIR/secrets/.env" ]]; then
+  source "$DIR/secrets/.env"
+else
+  echo "Missing: .env\n  Create it by copying the sample:\n  .env.sample -> .env"
+fi
--- work/setup.sh
+++ work/setup.sh
@@ -2,7 +2,11 @@ DIR="$(cd "$(dirname "${(%):-%x}")" && pwd)"
 
 source "$DIR/config/exports.sh"
 source "$DIR/config/aliases/projects.sh"
-source "$DIR/secrets/.env"
+if [[ -f "$DIR/secrets/.env" ]]; then
+  source "$DIR/secrets/.env"
+else
+  echo "Missing: .env\n  Create it by copying the sample:\n  .env.sample -> .env"
+fi
 
 # Trust Bitdefender SSL cert on iOS simulator
 xcrun simctl keychain booted add-root-cert ~/.bitdefender-ca.pem 2>/dev/null || true
