afc62f4783dac5dc2dcbb9665b534adba87aa2b7
Arman Mikoyan <arman.mikoyan@hearst.com>
2026-03-13 16:14:11 +0400

    feat: add sim-cert alias in work scope

---
+++ work/config/aliases/shell.sh
@@ -0,0 +1,1 @@
+alias trust-sim-cert='xcrun simctl keychain booted add-root-cert ~/.bitdefender-ca.pem'
--- work/setup.sh
+++ work/setup.sh
@@ -5,9 +5,7 @@ source "$DOTFILES_DIR/work/config/exports.sh"
 
 # Aliases
 source "$DOTFILES_DIR/work/config/aliases/goto.sh"
+source "$DOTFILES_DIR/work/config/aliases/shell.sh"
 
 # Secrets
 source "$DOTFILES_DIR/work/secrets/setup.sh"
-
-# iOS dev — run manually when simulator is booted
-alias trust-sim-cert='xcrun simctl keychain booted add-root-cert ~/.bitdefender-ca.pem'
